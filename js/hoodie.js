2dde14e5b79d78d18f5b8cb198fb2f2e0358f500
Craig Waterman <craigwaterman@gmail.com>
2011-10-06 17:05:53 -0400

    Mass cleanup to remove all the bootstrap cruft we don't want.

--- js/hoodie.js
+++ js/hoodie.js
@@ -3,10 +3,6 @@
  * @depends modernizr-2.0.6.min.js
  * @depends imgsizer.js
  * @depends bootstrap-alerts.js
- * @depends bootstrap-dropdown.js
- * @depends bootstrap-modal.js
  * @depends bootstrap-popover.js
- * @depends bootstrap-scrollspy.js
- * @depends bootstrap-tabs.js
  * @depends bootstrap-twipsy.js
- */
+ */ 
