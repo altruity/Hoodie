9eabbb571a385dc4351981fd4e5d76192d34e77f
Craig Waterman <craigwaterman@gmail.com>
2011-10-10 12:18:42 -0400

    Javascript and CSS fixes and cleanup
    Rejiggered to use LABjs for script loading, removed more conflicts from
    the CSS and cleaned up anything that was broken or ugly. The framework
    is now in a "usable" state. But not pretty or ready for release.

--- js/hoodie.js
+++
@@ -1,8 +0,0 @@
-/**
- * @depends selectivizr-min.js
- * @depends modernizr-2.0.6.min.js
- * @depends imgsizer.js
- * @depends bootstrap-alerts.js
- * @depends bootstrap-popover.js
- * @depends bootstrap-twipsy.js
- */ 
