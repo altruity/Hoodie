244277c80eecca67c17d7d24013f65bddf89fc2b
Craig Waterman <craigwaterman@gmail.com>
2011-12-15 19:47:23 -0500

    Began re-arranging the files.

---
+++ js/common.js
@@ -0,0 +1,8 @@
+// #########################################################################
+// Fill with love.
+$(document).ready( DomIsReady );
+
+function DomIsReady() {
+  // The world is your oyster.
+}
+
