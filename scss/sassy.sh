c63517d88fe8e02883f31078b6aef7747a87d53b
Craig Waterman <craigwaterman@gmail.com>
2011-12-15 20:38:13 -0500

    Finished moving the files around; added tabs/pills CSS back in

--- scss/sassy.sh
+++ scss/sassy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sass --style compressed hoodie.scss ../hoodie.min.css
-sass hoodie.scss ../hoodie.css
+sass -t compressed _hoodie.scss ../css/hoodie.min.css
+sass -l _hoodie.scss ../css/hoodie.css
